352a915a7e1982609235b6c22f841d45f9117aaf
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-05 20:10:10 +0500

    splash screen

--- application/resources/views/errors/419.blade.php
+++ application/resources/views/errors/419.blade.php
@@ -39,7 +39,7 @@
 <body>
 
 <!--==================== Preloader Start ====================-->
-<div id="loading">
+{{-- <div id="loading">
    <div id="loading-center">
       <div id="loading-center-absolute">
          <div class="object" id="object_one"></div>
@@ -48,7 +48,7 @@
          <div class="object" id="object_four"></div>
       </div>
    </div>
-</div>
+</div> --}}
 <!--==================== Preloader End ====================-->
 
 <!--==================== Overlay Start ====================-->
